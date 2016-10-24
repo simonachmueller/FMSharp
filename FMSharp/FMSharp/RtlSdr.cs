b15c06a45e76bef6294a33e3c4f12ef424fa43b7
cemen <cemen@ACERSMALL>
2016-10-24 22:02:57 +0200

    Dump device serial number

--- FMSharp/FMSharp/RtlSdr.cs
+++ FMSharp/FMSharp/RtlSdr.cs
@@ -18,7 +18,13 @@ namespace FMSharp
             List<SDRDevice> devices = new List<SDRDevice>((int)deviceCount);
             for (uint devIndex = 0; devIndex < deviceCount; devIndex++)
             {
-                devices.Add(new SDRDevice(RtlSdrWrapper.rtlsdr_get_device_name(devIndex), devIndex));
+                StringBuilder manufact = new StringBuilder(256);
+                StringBuilder product = new StringBuilder(256);
+                StringBuilder serial = new StringBuilder(256);
+                RtlSdrWrapper.rtlsdr_get_device_usb_strings(devIndex, manufact, product, serial);
+                string usbStrings;
+                usbStrings = string.Format("{0}, {1}, SN: {2}", manufact, product, serial);
+                devices.Add(new SDRDevice(RtlSdrWrapper.rtlsdr_get_device_name(devIndex), devIndex, usbStrings));
             }
             return devices.AsReadOnly();
         }
--- FMSharp/FMSharp/SDRDevice.cs
+++ FMSharp/FMSharp/SDRDevice.cs
@@ -10,14 +10,16 @@ namespace FMSharp
     {
         public string DeviceName { get; private set; }
         public uint DeviceIndex { get; private set; }
-        public SDRDevice(string deviceName, uint deviceIndex)
+        public string DeviceUSBStrings { get; private set; }
+        public SDRDevice(string deviceName, uint deviceIndex, string deviceUSBStrings)
         {
             DeviceName = deviceName;
             DeviceIndex = deviceIndex;
+            DeviceUSBStrings = deviceUSBStrings;
         }
         public override string ToString()
         {
-            return String.Format("ID {0}, {1}", DeviceIndex, DeviceName);
+            return String.Format("ID {0}, {1}, {2}", DeviceIndex, DeviceName, DeviceUSBStrings);
         }
     }
 }
